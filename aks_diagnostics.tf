0579bad25809b5c2f8d81cfc14a264ee4ca3b2eb
melvinlee <melvinlee@hotmail.com>
2019-10-04 23:22:27 +0800

    refactor diagnostic setting with map.

--- aks_diagnostics.tf
+++ aks_diagnostics.tf
@@ -9,22 +9,26 @@ resource "azurerm_monitor_diagnostic_setting" "aks_diag" {
   storage_account_id             = var.diagnostics_map.diags_sa
 
   dynamic "log" {
-    for_each = toset(var.diagnostics_log_category)
+    for_each = var.diagnostics_logs_map.log
     content {
-      category = log.key
+      category = log.value[0]
+      enabled  = log.value[1]
       retention_policy {
-        days    = var.opslogs_retention_period
-        enabled = true
+        enabled = log.value[2]
+        days    = log.value[3]
       }
     }
   }
 
-  metric {
-    category = "AllMetrics"
-
-    retention_policy {
-      days    = var.opslogs_retention_period
-      enabled = true
+  dynamic "metric" {
+    for_each = var.diagnostics_logs_map.metric
+    content {
+      category = metric.value[0]
+      enabled  = metric.value[1]
+      retention_policy {
+        enabled = metric.value[2]
+        days    = metric.value[3]
+      }
     }
   }
 }
