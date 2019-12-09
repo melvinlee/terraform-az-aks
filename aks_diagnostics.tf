27e6c94e0c0184e676c6f1af8591bebebd0e029f
melvinlee <melvinlee@hotmail.com>
2019-12-09 11:08:49 +0800

    refactored diagnostics settings.

--- aks_diagnostics.tf
+++ aks_diagnostics.tf
@@ -1,9 +1,11 @@
 resource "azurerm_monitor_diagnostic_setting" "aks_diag" {
 
+  count                          = (var.diagnostics_map.log_analytics_workspace_id != "" || lookup(var.diagnostics_map, "eh_name", null) != null) ? 1 : 0
+
   name                           = "${azurerm_kubernetes_cluster.aks.name}-diag"
   target_resource_id             = azurerm_kubernetes_cluster.aks.id
   eventhub_name                  = lookup(var.diagnostics_map, "eh_name", null)
-  eventhub_authorization_rule_id = length(lookup(var.diagnostics_map, "eh_id","")) > 1 ? "${var.diagnostics_map.eh_id}/authorizationrules/RootManageSharedAccessKey" : null
+  eventhub_authorization_rule_id = length(lookup(var.diagnostics_map, "eh_id", "")) > 1 ? "${var.diagnostics_map.eh_id}/authorizationrules/RootManageSharedAccessKey" : null
   log_analytics_workspace_id     = var.diagnostics_map.log_analytics_workspace_id
   storage_account_id             = lookup(var.diagnostics_map, "diags_sa", null)
 
